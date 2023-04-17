060a09d7a06fa5090746c276609fbdf93825b0bc
Sabbadini10 <matiassabbadini24@gmail.com>
2023-04-17 17:02:35 -0300

    fix hamburguer button

--- src/app/shared/component/navbar/navbar.component.ts
+++ src/app/shared/component/navbar/navbar.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  showMenu = false;
 
+  toggleMenu() {
+    this.showMenu = !this.showMenu;
+  }
+
+  isActive = false;
 }
+
